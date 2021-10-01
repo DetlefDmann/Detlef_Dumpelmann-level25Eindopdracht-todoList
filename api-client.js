d3bbd22fcb82579b9a3e1dde522fba6ea0cf428c
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-10-01 14:42:21 +0200

    adapted to use localAPI instead of jasonbox.io

--- api-client.js
+++ api-client.js
@@ -1,40 +1,63 @@
-const url = "https://jsonbox.io/box_13a228df4b01d31b03cc";
+const url = "http://localhost:3000"; //"https://jsonbox.io/box_13a228df4b01d31b03cc";
 
 const getData = async () => {
-    const res = await fetch(url);
-    const data =await res.json();
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
     return data;
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const postData = async (todo) => {
+  try {
     const answer = await fetch(url, {
-        method:"POST",
-        body: JSON.stringify({
-            "description":todo.description,//alleen eigen data
-            "done": false   }), 
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
+      method: "POST",
+      body: JSON.stringify({
+        description: todo.description, //alleen eigen data
+        done: false,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return answer;
-
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 const putData = async (todo) => {
-    await fetch(`${url}/${todo._id}`, { //id
-        method:"PUT",
-        body: JSON.stringify({
-            "description":todo.description,
-            "done":todo.done}), //eigen data als object {hb}
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
-}
+  try {
+    await fetch(`${url}/${todo._id}`, {
+      //id
+      method: "PUT",
+      body: JSON.stringify({
+        description: todo.description,
+        done: todo.done,
+      }), //eigen data als object {hb}
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteData = async (todo) => {
+  try {
     await fetch(`${url}/${todo._id}`, {
-        method: "DELETE"
-    })
-}
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.log(`Delete didn't work because of error: ${error}`);
+  }
+};
 
-export {getData, postData, putData, deleteData};
+export { getData, postData, putData, deleteData };
